c0f5fb14dd082e115335be83c70611ff5ccc44f8
isadora-cmn <dora.neves270@gmail.com>
2025-08-27 02:32:07 -0300

    feito ContaCorrente e Pessoa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AppSaldo.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Isa";
+// p1.Idade = 18;
+
+// p1.Apresentar();
